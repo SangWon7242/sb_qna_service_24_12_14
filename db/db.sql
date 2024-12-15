859aed35b94a346011965309af3ee94ad895d237
SangWon7242 <gotkdqja@gmail.com>
2024-12-15 10:37:01 +0900

    SQL SELECT

--- db/db.sql
+++ db/db.sql
@@ -3,12 +3,12 @@ DROP DATABASE IF EXISTS qna_service;
 CREATE DATABASE qna_service;
 USE qna_service;
 
-// 외래키 제약 비활성화
+# 외래키 제약 비활성화
 SET FOREIGN_KEY_CHECKS = 0;
 
-// TRUNCATE 로 초기화한 데이터는 데이터 추가시 1번 번호로 시작한다.
+# TRUNCATE 로 초기화한 데이터는 데이터 추가시 1번 번호로 시작한다.
 TRUNCATE answer;
 TRUNCATE question;
 
-// 외래키 제약 활성화
+# 외래키 제약 활성화
 SET FOREIGN_KEY_CHECKS = 1;
