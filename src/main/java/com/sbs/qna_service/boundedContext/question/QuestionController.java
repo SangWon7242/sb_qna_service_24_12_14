5d0ffe33cb691ec9dfcca1682c2bf79b0566be67
SangWon7242 <gotkdqja@gmail.com>
2024-12-28 11:15:30 +0900

    question_form.html 이 오류때문에 다시 실행될 때 기존 값 유지

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,7 +34,8 @@ public class QuestionController {
   }
 
   @GetMapping("/create")
-  public String questionCreate(QuestionForm questionForm) {
+  public String questionCreate(Model model) {
+    model.addAttribute("questionForm", new QuestionForm());
     return "question_form";
   }
 
