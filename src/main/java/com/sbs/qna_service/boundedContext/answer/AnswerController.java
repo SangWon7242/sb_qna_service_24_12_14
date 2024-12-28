335bd61064eac3cad8c0befbae8dc74ff5066a83
SangWon7242 <gotkdqja@gmail.com>
2024-12-28 11:29:07 +0900

    답변등록에 폼 유효성 체크 적용

--- src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
@@ -2,9 +2,11 @@ package com.sbs.qna_service.boundedContext.answer;
 
 import com.sbs.qna_service.boundedContext.question.Question;
 import com.sbs.qna_service.boundedContext.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/answer")
@@ -15,11 +17,16 @@ public class AnswerController {
   private final AnswerService answerService;
 
   @PostMapping("/create/{id}")
-  public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content) {
+  public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
     Question question = questionService.getQuestion(id);
 
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("question", question);
+      return "question_detail";
+    }
+
     // TODO: 답변을 저장한다.
-    Answer answer = answerService.create(question, content);
+    Answer answer = answerService.create(question, answerForm.getContent());
 
     // return "%d번 질문에 대한 답변이 생성되었습니다.(답변 번호 : %d)".formatted(id, answer.getId());
 
