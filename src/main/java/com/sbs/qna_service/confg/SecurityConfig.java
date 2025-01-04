de10a5517c5e7c001a26609d5f6a08f6343ad167
SangWon7242 <gotkdqja@gmail.com>
2025-01-04 10:29:58 +0900

    로그인

--- src/main/java/com/sbs/qna_service/confg/SecurityConfig.java
+++ src/main/java/com/sbs/qna_service/confg/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.sbs.qna_service.confg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,11 +15,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    /*
+    http
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+            .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+    ; // /question/list는 인증 없이 접근이 가능
+     */
+
+    http.formLogin((formLogin) -> formLogin
+        .loginPage("/user/login") // 로그인 폼으로 이동
+        .loginProcessingUrl("/user/login") // 로그인 처리시 요청 경로
+        .defaultSuccessUrl("/")); // 로그인 성공시 리다이렉트 경로
+
     return http.build();
   }
 
   @Bean
-  PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+  
+  @Bean
+  // 스프링 시큐리티 인증을 처리
+  // 커스텀 인증 로직을 구현할 때 필요
+  AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }
