aa9d2e21702b435b6d49bcd51ed676c3d4c37d87
SangWon7242 <gotkdqja@gmail.com>
2025-01-04 11:38:43 +0900

    로그아웃 기능 구현

--- src/main/java/com/sbs/qna_service/confg/SecurityConfig.java
+++ src/main/java/com/sbs/qna_service/confg/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
             .requestMatchers(new AntPathRequestMatcher("/question/detail/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/user/signup")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/user/login")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/style.css")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
             .anyRequest().authenticated() // 그 외의 요청은 인증이 필요!
         )
@@ -31,16 +32,21 @@ public class SecurityConfig {
             .usernameParameter("name") // 아이디 필드 이름 변경
             .passwordParameter("pass") // 비밀번호 필드 이름 변경
              */
-            
+
             // GET
             // 시큐리티에게 우리가 만든 로그인 페이지 URL을 알려준다.
             // 만약에 하지 않으면 기본 로그인 페이지 url은 /login 이다.
-        .loginPage("/user/login") // 로그인 폼으로 이동
+            .loginPage("/user/login") // 로그인 폼으로 이동
 
             // POST
             // 시큐리티에게 로그인 폼 처리 url을 알려준다.
-        .loginProcessingUrl("/user/login") // 로그인 처리시 요청 경로
-        .defaultSuccessUrl("/")); // 로그인 성공시 리다이렉트 경로
+            .loginProcessingUrl("/user/login") // 로그인 처리시 요청 경로
+            .defaultSuccessUrl("/")) // 로그인 성공시 리다이렉트 경로
+        .logout((logout) -> logout
+            // .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+            .logoutUrl("/user/logout")
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true));
 
     return http.build();
   }
@@ -49,10 +55,10 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-  
+
   @Bean
-  // 스프링 시큐리티 인증을 처리
-  // 커스텀 인증 로직을 구현할 때 필요
+    // 스프링 시큐리티 인증을 처리
+    // 커스텀 인증 로직을 구현할 때 필요
   AuthenticationManager authenticationManager(
       AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
