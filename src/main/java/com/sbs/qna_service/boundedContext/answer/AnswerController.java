8a2debb3c3d83093567833591d8ed6377f5170da
SangWon7242 <gotkdqja@gmail.com>
2025-01-05 09:26:52 +0900

    authorf 엔티티 추가

--- src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
@@ -2,12 +2,19 @@ package com.sbs.qna_service.boundedContext.answer;
 
 import com.sbs.qna_service.boundedContext.question.Question;
 import com.sbs.qna_service.boundedContext.question.QuestionService;
+import com.sbs.qna_service.boundedContext.user.SiteUser;
+import com.sbs.qna_service.boundedContext.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -15,10 +22,21 @@ import org.springframework.web.bind.annotation.*;
 public class AnswerController {
   private final QuestionService questionService;
   private final AnswerService answerService;
+  private final UserService userService;
 
+  @PreAuthorize("isAuthenticated()")
   @PostMapping("/create/{id}")
-  public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
+  public String createAnswer(
+      Model model,
+      @PathVariable("id") Integer id,
+      @Valid AnswerForm answerForm,
+      BindingResult bindingResult,
+      Principal principal
+  ) {
     Question question = questionService.getQuestion(id);
+
+    // principal.getName() : 글 작성자를 가져온다.
+    SiteUser siteUser = userService.getUser(principal.getName());
 
     if (bindingResult.hasErrors()) {
       model.addAttribute("question", question);
@@ -26,7 +44,7 @@ public class AnswerController {
     }
 
     // TODO: 답변을 저장한다.
-    Answer answer = answerService.create(question, answerForm.getContent());
+    Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
 
     // return "%d번 질문에 대한 답변이 생성되었습니다.(답변 번호 : %d)".formatted(id, answer.getId());
 
--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
@@ -1,19 +1,25 @@
 package com.sbs.qna_service.boundedContext.question;
 
 import com.sbs.qna_service.boundedContext.answer.AnswerForm;
+import com.sbs.qna_service.boundedContext.user.SiteUser;
+import com.sbs.qna_service.boundedContext.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
   private final QuestionService questionService;
+  private final UserService userService;
 
   @GetMapping("/list")
   public String list(Model model,  @RequestParam(defaultValue = "0") int page) {
@@ -38,17 +44,24 @@ public class QuestionController {
     return "question_form";
   }
 
+  @PreAuthorize("isAuthenticated()")
   @PostMapping("/create")
   // @Valid QuestionForm questionForm
   // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
-  public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+  public String questionCreate(
+      @Valid QuestionForm questionForm,
+      BindingResult bindingResult,
+      Principal principal
+  ) {
     if (bindingResult.hasErrors()) { // hasErrors : 에러가 존재한다면 true, 존재하지 않으면 false
       // question_form.html 실행
       // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
       return "question_form";
     }
-    
-    questionService.create(questionForm.getSubject(), questionForm.getContent());
+
+    SiteUser siteUser = userService.getUser(principal.getName());
+    questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+
     return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
   }
 }
